ce1629f3d1ca4843ae88afd7a5c2ad1c44b73799
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-09 16:38:45 +0400

    commit

---
+++ letter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Isletter_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World";
+            char word = 'A';
+            Console.WriteLine(HasLetter(text, word));
+        }
+        static bool HasLetter(string text,char word)
+        {
+            
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == word)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
